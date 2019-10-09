4239103f983f8aceb8354ba4a112b3728980c854
icpengineer <icpengineer@intcrzyppl.com>
2019-10-09 16:15:55 +0100

    completed stock prices

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,8 +3,22 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  small_price = prices[0]
+  largest_profit = prices[1] - prices[0]
+  print(largest_profit)
+  # loop through to find the smallest item in the array that is not the last
+  for lowest in range(0,len(prices) - 1):
+        if prices[lowest] != small_price and prices[lowest] - small_price > largest_profit:
+              largest_profit = prices[lowest] - small_price
+              
+        if prices[lowest] < small_price:
+              small_price = prices[lowest]
+        
+  return largest_profit
+              
 
+        
+print(find_max_profit([100, 90, 80, 50, 20, 10]))
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
